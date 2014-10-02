28b0cae8b825a60249b44078d1e46b7b6d0cddcc
halfak <aaron.halfaker@gmail.com>
2014-10-02 09:52:55 -0500

    Adds missing util file.:

---
+++ deltas/tests/diff_and_replay.py
@@ -0,0 +1,31 @@
+from nose.tools import eq_
+
+from ..apply import apply
+from ..tokenizers import TextSplit
+
+
+def diff_and_replay(diff, tokenizer=None):
+    a = """
+    This sentence is going to get copied. This sentence is going to go away.
+    
+    ASDSJDNA  asas random words
+    
+    This is another sentence.
+    """
+    
+    b = """
+    This sentence is going to get copied.  Wha... a new thing appeared!
+    
+    ASDSJDNA  asas random words
+    
+    This is another sentence. This sentence is going to get copied.
+    """
+    tokenizer = tokenizer or TextSplit()
+    a_tokens = tokenizer.tokenize(a)
+    b_tokens = tokenizer.tokenize(b)
+    operations = list(diff(a_tokens, b_tokens))
+    
+    replay_b = "".join(
+        str(t) for t in apply(operations, a_tokens, b_tokens)
+    )
+    eq_(b, replay_b)
