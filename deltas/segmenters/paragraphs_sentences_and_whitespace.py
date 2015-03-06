2702b97e09ccc92bedb3d33d3b8108c3f40e87b4
halfak <aaron.halfaker@gmail.com>
2015-03-06 17:39:27 -0500

    Removes unnecessary dependencies from segmenter and token.

--- deltas/segmenters/paragraphs_sentences_and_whitespace.py
+++ deltas/segmenters/paragraphs_sentences_and_whitespace.py
@@ -1,7 +1,5 @@
 import re
 
-from more_itertools import peekable
-
 from ..util import LookAhead
 from .segmenter import Segmenter
 from .segments import MatchableSegment, Segment
@@ -16,14 +14,14 @@ class ParagraphsSentencesAndWhitespace(Segmenter):
     Constructs a paragraphs, sentences and whitespace segmenter.  This segmenter
     is intended to be used in western languages where sentences and paragraphs
     are meaningful segments of text content.
-    
+
     Tree structure:
-    
+
     * whitespace :class:`~deltas.segmenters.Segment`
     * paragraph :class:`~deltas.segmenters.MatchableSegment` which contains
      * sentence :class:`~deltas.segmenters.MatchableSegment`
      * whitespace :class:`~deltas.segmenters.Segment`
-    
+
     :Parameters:
         whitespace : `set`(`str`)
             A set of token types that represent whitespace.
@@ -39,16 +37,16 @@ class ParagraphsSentencesAndWhitespace(Segmenter):
                           paragraph_end=None,
                           sentence_end=None,
                           min_sentence=None):
-        
+
         self.whitespace = set(whitespace or WHITESPACE)
         self.paragraph_end = set(paragraph_end or PARAGRAPH_END)
         self.sentence_end = set(sentence_end or SENTENCE_END)
         self.min_sentence = int(min_sentence or MIN_SENTENCE)
-    
+
     def segment(self, tokens):
         """
         Segments a sequence of tokens into a sequence of segments.
-        
+
         :Parameters:
             tokens : `list`(`deltas.tokenizers.Token`)
         """
@@ -57,41 +55,41 @@ class ParagraphsSentencesAndWhitespace(Segmenter):
         segments = []
 
         while not look_ahead.empty():
-            
+
             if look_ahead.peek().type not in self.whitespace: # Paragraph!
                 paragraph = MatchableSegment()
-                
+
                 while not look_ahead.empty() and \
                       look_ahead.peek().type not in self.paragraph_end:
 
                     if look_ahead.peek().type not in self.whitespace: #Sentence!
                         sentence = MatchableSegment([next(look_ahead)])
-                        
+
                         while not look_ahead.empty() and \
                               look_ahead.peek().type not in self.paragraph_end:
-                            
+
                             sentence.append(next(look_ahead))
-                            
+
                             if sentence[-1].type in self.sentence_end:
                                 non_whitespace = sum(s.type not in self.whitespace for s in sentence)
                                 if non_whitespace >= self.min_sentence:
                                     break
-                            
-                        
+
+
                         paragraph.append(sentence)
-                        
+
                     else: # look_ahead.peek().type in self.whitespace
                         whitespace = Segment([next(look_ahead)])
                         paragraph.append(whitespace)
-                
+
                 segments.append(paragraph)
             else: # look_ahead.peek().type in self.whitespace
                 whitespace = Segment([next(look_ahead)])
                 segments.append(whitespace)
-            
-        
+
+
         return segments
-    
+
     @classmethod
     def from_config(cls, config, name, section_key="segmenters"):
         section = config[section_key][name]
--- deltas/tokenizers/token.py
+++ deltas/tokenizers/token.py
@@ -5,38 +5,37 @@ extends :class:`str` is profivided.
 .. autoclass:: deltas.tokenizers.Token
     :members:
 """
-from jsonable import JSONable
 
 
 class Token(str):
-    
+
     __slots__ = ("type", "i")
-    
+
     def __new__(cls, content, *args, **kwargs):
         if isinstance(content, cls):
             return content
         else:
             return super().__new__(cls, content)
-    
+
     def __init__(self, content, i, type=None):
         super().__init__()
         self.type = str(type) if type is not None else None
         self.i = int(i)
-    
+
     def __str__(self):
         return super().__str__()
-    
+
     def __repr__(self):
         return "{0}({1}, i={2}, type={3})" \
                .format(self.__class__.__name__,
                        super().__repr__(),
                        self.i,
                        repr(self.type))
-    
+
     @property
     def start(self):
         return self.i
-    
+
     @property
     def end(self):
         return self.i+1
