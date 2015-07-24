40d984d2bcceb5fc4f36b42c350c07810fe1971b
halfak <aaron.halfaker@gmail.com>
2015-07-24 14:02:19 -0500

    Adds url to types of tokens for wikitext_split and implements test.

--- deltas/tokenizers/tests/test_wikitext_split.py
+++ deltas/tokenizers/tests/test_wikitext_split.py
@@ -4,19 +4,22 @@ from ..wikitext_split import wikitext_split
 
 
 def test_wikitext_split():
-    
-    
+
+
     input = "As a sentence, this includes punctuation. \n" + \
             "\n" + \
-            "==Header!==" + \
-            "And then we have another sentence here!"
-    
+            "==Header!==\n" + \
+            "And then we have another sentence here!\n" + \
+            "https://website.gov?param=value\n" + \
+            "mailto:email@email.mail"
+
     expected = ['As', ' ', 'a', ' ', 'sentence', ',', ' ', 'this', ' ',
                 'includes', ' ', 'punctuation', '.', ' \n\n',
-                '==', 'Header', '!', '==', 'And', ' ',
+                '==', 'Header', '!', '==', '\n', 'And', ' ',
                 'then', ' ', 'we', ' ', 'have', ' ', 'another', ' ', 'sentence',
-                ' ', 'here', '!']
-    
+                ' ', 'here', '!', '\n', 'https://website.gov?param=value', '\n',
+                'mailto:email@email.mail']
+
     tokens = list(wikitext_split.tokenize(input))
-    
+
     eq_(tokens, expected)
--- deltas/tokenizers/wikitext_split.py
+++ deltas/tokenizers/wikitext_split.py
@@ -2,9 +2,48 @@ import re
 
 from .tokenizer import RegexTokenizer
 
+PLAIN_PROTO = [r'bitcoin', r'geo', r'magnet', r'mailto', r'news', r'sips?',
+               r'tel', r'urn']
+SLASHED_PROTO = [r'', r'ftp', r'ftps', r'git', r'gopher', r'https?', r'ircs?',
+                 r'mms', r'nntp', r'redis', r'sftp', r'ssh', r'svn', r'telnet',
+                 r'worldwind', r'xmpp']
+ADDRESS = (
+    r'(?:\S+(?::\S*)?@)?' +
+    r'(?:' +
+        r'(?!10(?:\.\d{1,3}){3})' +
+        r'(?!127(?:\.\d{1,3}){3})' +
+        r'(?!169\.254(?:\.\d{1,3}){2})' +
+        r'(?!192\.168(?:\.\d{1,3}){2})' +
+        r'(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})' +
+        r'(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])' +
+        r'(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}' +
+        r'(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|' +
+        r'(?:' +
+            r'(?:[a-z\u00a1-\uffff0-9]+-?)*' +
+            r'[a-z\u00a1-\uffff0-9]+' +
+        r')' +
+        r'(?:' +
+            r'\.(?:[a-z\u00a1-\uffff0-9]+-?)*' +
+            r'[a-z\u00a1-\uffff0-9]+' +
+        r')*' +
+        r'(?:\.(?:[a-z\u00a1-\uffff]{2,}))?' +
+    r')' +
+    r'(?::\d{2,5})?' +
+    r'(?:[^a-z\u00a1-\uffff0-9][^\s]*)?'
+)
+url = (
+    r'(' + \
+        r'(' + '|'.join(PLAIN_PROTO) + r')\:|' + \
+        r'(' + '|'.join(SLASHED_PROTO) + r')\:\/\/' + \
+    r')' + ADDRESS
+)
+re.compile(url, re.U).match("https://website.gov?param=value")
+
+
 LEXICON = [
     ('comment_start', r'<!--'),
     ('comment_end',   r'-->'),
+    ("url",           url),
     ('entity',        r'&[a-z][a-z0-9]*;'),
     ('word',          r'[^[\W\d]+'),
     ('number',        r'[\d]+'),
