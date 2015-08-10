f1d2dd9c552bb2abf7275cf791c1cb242a22dcdb
halfak <aaron.halfaker@gmail.com>
2015-08-10 14:29:40 -0500

    Increments version to 0.3.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def requirements(fname):
 
 setup(
     name = "deltas",
-    version = "0.3.1",
+    version = "0.3.2",
     author = "Aaron Halfaker",
     author_email = "ahalfaker@wikimedia.org",
     description = "An experimental diff library for generating " + \
