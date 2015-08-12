f738f90c5f7906572316388b03c3c862a77782c4
halfak <aaron.halfaker@gmail.com>
2015-08-11 19:02:28 -0500

    Adds word-level handling of apostrophes in wikitext_split.

--- deltas/tokenizers/tests/test_wikitext_split.py
+++ deltas/tokenizers/tests/test_wikitext_split.py
@@ -12,17 +12,20 @@ def test_wikitext_split():
             "克·科伊尔 foobar!" + \
             "And then we have another sentence here!\n" + \
             "https://website.gov?param=value\n" + \
+            "peoples' ain't d’encyclopédie\n" + \
             "mailto:email@email.mail"
 
-    expected = ['As', ' ', 'a', ' ', 'sentence', ',', ' ', 'this', ' ',
-                'includes', ' ', 'punctuation', '.', ' \n\n',
-                '==', 'Header', '!', '==', '\n',
-                '克', '·', '科', '伊', '尔', ' ', 'foobar', '!',
-                'And', ' ', 'then', ' ', 'we', ' ', 'have', ' ', 'another',
-                ' ', 'sentence',
-                ' ', 'here', '!', '\n', 'https://website.gov?param=value', '\n',
-                'mailto:email@email.mail']
+    expecteds = ['As', ' ', 'a', ' ', 'sentence', ',', ' ', 'this', ' ',
+                 'includes', ' ', 'punctuation', '.', ' \n\n',
+                 '==', 'Header', '!', '==', '\n',
+                 '克', '·', '科', '伊', '尔', ' ', 'foobar', '!',
+                 'And', ' ', 'then', ' ', 'we', ' ', 'have', ' ', 'another',
+                 ' ', 'sentence',
+                 ' ', 'here', '!', '\n', 'https://website.gov?param=value', '\n',
+                 'peoples\'', ' ', 'ain\'t', ' ', 'd’encyclopédie', '\n',
+                 'mailto:email@email.mail']
 
     tokens = list(wikitext_split.tokenize(input))
 
-    eq_(tokens, expected)
+    for token, expected in zip(tokens, expecteds):
+        eq_(token, expected)
