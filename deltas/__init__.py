874fd0725cb6252a280d29d52238b2c862e07d07
halfak <aaron.halfaker@gmail.com>
2015-12-13 18:10:53 -0600

    Increments version to 0.3.8

--- deltas/__init__.py
+++ deltas/__init__.py
@@ -8,7 +8,7 @@ from .tokenizers import (Token, Tokenizer, RegexTokenizer, text_split,
 from .segmenters import (Segmenter, Segment, MatchableSegment,
                          ParagraphsSentencesAndWhitespace)
 
-__version__ = "0.3.7"
+__version__ = "0.3.8"
 
 __all__ = [apply,
            Operation, Insert, Delete, Equal,
