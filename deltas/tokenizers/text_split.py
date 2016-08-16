ebb5457c5166f7f1a0143ab1f1538d1df3391b89
halfak <aaron.halfaker@gmail.com>
2016-08-16 14:46:46 -0500

    Minor fix to text_split

--- deltas/tokenizers/text_split.py
+++ deltas/tokenizers/text_split.py
@@ -1,14 +1,14 @@
 from .tokenizer import RegexTokenizer
 
 LEXICON = [
-    ('word',          r'[^[\W\d]+'),
+    ('word',          r'[^\W\d]+'),
     ('number',        r'[\d]+'),
-    ('period',        r'\.+'),
-    ('qmark',         r'\?+'),
-    ('epoint',        r'!+'),
-    ('comma',         r',+'),
-    ('colon',         r':+'),
-    ('scolon',        r';+'),
+    ('period',        r'\.'),
+    ('qmark',         r'\?'),
+    ('epoint',        r'!'),
+    ('comma',         r','),
+    ('colon',         r':'),
+    ('scolon',        r';'),
     ('break',         r'(\n|\n\r|\r\n)\s*(\n|\n\r|\r\n)+'),
     ('whitespace',    r'[\n\r\s]+'),
     ("etc",           r".")
