c9833b8063c6a83fa44a46d1f7c371d283e1e0a6
Léo Joubert <joubert.leo@gmail.com>
2019-03-25 07:58:06 +0100

    Revert "handling errors"
    This reverts commit 86e74d265c05457d0aec412f5b64812a0cc81ce2.

--- deltas/apply_get_b.py
+++ deltas/apply_get_b.py
@@ -1,27 +1,21 @@
-from pprint import pprint
-
 def apply_get_b(operations_diff_file):
 
-    try:
+    length_b = max([operation["b2"] for operation in operations_diff_file])
+    b = [''] * length_b
 
-        length_b = max([operation["b2"] for operation in operations_diff_file])
-        b = [''] * length_b
+    for operation in operations_diff_file:
 
-        for operation in operations_diff_file:
+        if operation["name"] == "equal" or operation["name"] == "insert":
+            #print("Equal: {0}".format(str(a_tokens[operation.a1:operation.a2])))
+            if "tokens" in operation.keys():
+                b[operation["b1"]:operation["b2"]] = operation["tokens"]
 
-            if operation["name"] == "equal" or operation["name"] == "insert":
-                #print("Equal: {0}".format(str(a_tokens[operation.a1:operation.a2])))
-                if "tokens" in operation.keys():
-                    b[operation["b1"]:operation["b2"]] = operation["tokens"]
+        elif operation["name"] == "delete":
+            #print("Insert: {0}".format(str(b_tokens[operation.b1:operation.b2])))
+            pass
 
-            elif operation["name"] == "delete":
-                #print("Insert: {0}".format(str(b_tokens[operation.b1:operation.b2])))
-                pass
+        else:
+            raise TypeError("Unexpected operation type " + \
+                            "{0}".format(type(operation)))
 
-            else:
-                raise TypeError("Unexpected operation type " + \
-                                "{0}".format(type(operation)))
-
-        return ' '.join(b)
-    except:
-        pprint(operations_diff_file)
+    return ' '.join(b)
