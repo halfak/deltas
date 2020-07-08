3648d9a4901729120e8a0a0cf7662b1de142d90b
5uperpalo <palusko@gmail.com>
2020-07-09 00:50:24 +0200

    cjk tokenization strategy - without error handling

--- deltas/tokenizers/tokenizer.py
+++ deltas/tokenizers/tokenizer.py
@@ -3,6 +3,9 @@ import re
 import yamlconf
 
 from .token import Token
+import pkuseg
+from konlpy.tag import Mecab as ko_mecab
+import MeCab as jp_mecab
 
 
 class Tokenizer:
@@ -29,13 +32,53 @@ class RegexTokenizer(Tokenizer):
     """
     Uses a lexicon of regular expressions and names to tokenize a text string.
     """
-    def __init__(self, lexicon):
+    def __init__(self, lexicon, cjk_lexicon=None):
         self.lexicon = lexicon
         self.regex = re.compile('|'.join('(?P<{0}>{1})'.format(name, pattern)
                                          for name, pattern in lexicon))
+        if cjk_lexicon is not None:
+            self.regex_cjk = re.compile(cjk_lexicon['cjk'])
+            self.regex_japanese = re.compile(cjk_lexicon['japanese'])
+            self.regex_korean = re.compile(cjk_lexicon['korean'])
 
-    def tokenize(self, text, token_class=None):
-        return [t for t in self._tokenize(text, token_class=token_class)]
+    def tokenize(self, text, token_class=None, cjk=False):
+        tokenized_text = [t for t in self._tokenize(text, token_class=token_class)]
+        if cjk is True:
+            cjk_symbols = len(self.regex_cjk.findall(text))
+            jap_symbols = len(self.regex_japanese.findall(text))
+            kor_symbols = len(self.regex_korean.findall(text))
+            char_lang_count = {'cjk': 1,
+                               'japanese': 0.75 + jap_symbols/cjk_symbols,
+                               'korean': 0.75 + kor_symbols/cjk_symbols}
+            char_lang = max(char_lang_count, key=char_lang_count.get)
+            tokenized_text = self._cjk_tokenization(tokenized_text, language=char_lang, token_class=token_class)
+        return tokenized_text
+
+    def _cjk_tokenization(self, tokenized_text, language, token_class=None):
+        token_class = token_class or Token
+        cjk_word_indices = list(filter(lambda x: tokenized_text[x].type == 'cjk_word', range(len(tokenized_text))))
+
+        if language == 'cjk':
+            seg = pkuseg.pkuseg()
+            for i in cjk_word_indices[::-1]:
+                segmented_cjk_token = seg.cut(tokenized_text[i])
+                tokenized_text[i:i+1] = [token_class(word, type="cjk_word") for word in segmented_cjk_token]
+            return tokenized_text
+
+        if language == 'japanese':
+            seg = jp_mecab.Tagger("-Owakati")
+            for i in cjk_word_indices[::-1]:
+                segmented_cjk_token = seg.parse(tokenized_text[i]).split()
+                tokenized_text[i:i+1] = [token_class(word, type="cjk_word") for word in segmented_cjk_token]
+            return tokenized_text
+
+        if language == 'korean':
+            seg = ko_mecab()
+            for i in cjk_word_indices[::-1]:
+                segmented_cjk_token = seg.nouns(tokenized_text[i])
+                if segmented_cjk_token != []:
+                    tokenized_text[i:i+1] = [token_class(word, type="cjk_word") for word in segmented_cjk_token]
+            return tokenized_text
 
     def _tokenize(self, text, token_class=None):
         """
