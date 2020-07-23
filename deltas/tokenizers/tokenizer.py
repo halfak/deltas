1fd860d11212a6d00d0f85d35aa3837af69b9869
5uperpalo <palusko@gmail.com>
2020-07-23 16:55:21 +0200

    finished

--- deltas/tokenizers/tokenizer.py
+++ deltas/tokenizers/tokenizer.py
@@ -3,9 +3,11 @@ import re
 import yamlconf
 
 from .token import Token
-import pkuseg
-from konlpy.tag import Mecab as ko_mecab
-import MeCab as jp_mecab
+
+import jieba as ch_jieba
+from sudachipy import tokenizer as jp_tokenizer
+from sudachipy import dictionary as jp_dictionary
+from konlpy.tag import Okt as ko_okt
 
 
 class Tokenizer:
@@ -47,37 +49,41 @@ class RegexTokenizer(Tokenizer):
             cjk_symbols = len(self.regex_cjk.findall(text))
             jap_symbols = len(self.regex_japanese.findall(text))
             kor_symbols = len(self.regex_korean.findall(text))
-            char_lang_count = {'cjk': 1,
-                               'japanese': 0.75 + jap_symbols/cjk_symbols,
-                               'korean': 0.75 + kor_symbols/cjk_symbols}
-            char_lang = max(char_lang_count, key=char_lang_count.get)
-            tokenized_text = self._cjk_tokenization(tokenized_text, language=char_lang, token_class=token_class)
+            char_lang_frac = {'japanese': jap_symbols/cjk_symbols,
+                               'korean': kor_symbols/cjk_symbols}
+            max_char_lang_frac = max(char_lang_frac, key=char_lang_frac.get)
+            # check if at least 1/4 of chars are other than chinese, if not -> run chinese tokenizer
+            if char_lang_frac[max_char_lang_frac]>0.25:
+                tokenized_text = self._cjk_tokenization(tokenized_text, language=max_char_lang_frac, token_class=token_class)
+            else:
+                tokenized_text = self._cjk_tokenization(tokenized_text, language='cjk', token_class=token_class)
         return tokenized_text
 
     def _cjk_tokenization(self, tokenized_text, language, token_class=None):
         token_class = token_class or Token
         cjk_word_indices = list(filter(lambda x: tokenized_text[x].type == 'cjk_word', range(len(tokenized_text))))
-
+        
         if language == 'cjk':
-            seg = pkuseg.pkuseg()
+            seg = ch_jieba.initialize()
             for i in cjk_word_indices[::-1]:
-                segmented_cjk_token = seg.cut(tokenized_text[i])
+                segmented_cjk_token = ','.join(ch_jieba.cut(tokenized_text[i], cut_all=False))
                 tokenized_text[i:i+1] = [token_class(word, type="cjk_word") for word in segmented_cjk_token]
-            return tokenized_text
+            return tokenized_text        
 
         if language == 'japanese':
-            seg = jp_mecab.Tagger("-Owakati")
+            mode = jp_tokenizer.Tokenizer.SplitMode.B
+            seg = jp_dictionary.Dictionary().create()
             for i in cjk_word_indices[::-1]:
-                segmented_cjk_token = seg.parse(tokenized_text[i]).split()
+                segmented_cjk_token = [m.surface() for m in seg.tokenize(str(tokenized_text[i]), mode)]
                 tokenized_text[i:i+1] = [token_class(word, type="cjk_word") for word in segmented_cjk_token]
             return tokenized_text
 
         if language == 'korean':
-            seg = ko_mecab()
+            seg = ko_okt()
             for i in cjk_word_indices[::-1]:
                 segmented_cjk_token = seg.nouns(tokenized_text[i])
                 if segmented_cjk_token != []:
-                    tokenized_text[i:i+1] = [token_class(word, type="cjk_word") for word in segmented_cjk_token]
+                    tokenized_text[i:i+1] = [token_class(word, type="cjk_word") for word in segmented_cjk_token]            
             return tokenized_text
 
     def _tokenize(self, text, token_class=None):
