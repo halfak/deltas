2a24e817b751197fa43331e585f66161fa0936a3
Pavol Mulinka <mulinka.pavol@gmail.com>
2020-12-18 07:22:28 +0100

    flake8 fix

--- deltas/tokenizers/wikitext_split_w_cjk.py
+++ deltas/tokenizers/wikitext_split_w_cjk.py
@@ -3,5 +3,5 @@ from .tokenizer import CJKProcessor
 from .tokenizer import RegexTokenizer
 from . import lexicon
 
-wikitext_split_w_cjk = TokenizerPipeline(RegexTokenizer(lexicon.WIKITEXT_SPLIT_LEXICON),
+wikitext_split_w_cjk = TokenizerPipeline(RegexTokenizer(lexicon.WIKITEXT_SPLIT_LEXICON), # noqa
                                          CJKProcessor(lexicon.CJK_LEXICON))
