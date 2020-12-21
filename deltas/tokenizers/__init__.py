a52d1586d3631d1ee842d153ce865a324bb82024
Pavol Mulinka <mulinka.pavol@gmail.com>
2020-12-21 09:32:52 +0100

    cleaning

--- deltas/tokenizers/__init__.py
+++ deltas/tokenizers/__init__.py
@@ -12,12 +12,19 @@ based on a lexicon.
     a :class:`~deltas.RegexTokenizer` that splits text into words,
     punctuation, symbols and whitespace as well as wikitext markup elements
     (e.g. ('dcurly_open', "{{") and ('bold', "'''"))
+
+**deltas.wikitext_w_split**
+    a :class:`~deltas.TokenizerPipeline` that splits text into words,
+    punctuation, symbols and whitespace as well as wikitext markup elements
+    (e.g. ('dcurly_open', "{{") and ('bold', "'''")) and performs additional
+    tokenization of 'cjk_word' tokens
 """
 from .tokenizer import Tokenizer, RegexTokenizer, CJKProcessor, TokenizerPipeline # noqa
 from .token import Token
 from .text_split import text_split
 from .wikitext_split import wikitext_split
 from .wikitext_split_w_cjk import wikitext_split_w_cjk
+from .cjk_tokenization import lng_decision, CJK_tokenization
 
 
-__all__ = [Tokenizer, RegexTokenizer, CJKProcessor, TokenizerPipeline, Token, text_split, wikitext_split, wikitext_split_w_cjk] # noqa
+__all__ = [Tokenizer, RegexTokenizer, CJKProcessor, TokenizerPipeline, Token, text_split, wikitext_split, wikitext_split_w_cjk, lng_decision, CJK_tokenization] # noqa
